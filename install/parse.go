d853511e4201b2c1c4f5b9a9c464aa2972f55765
KasperJack <boubakeradem47@gmail.com>
2025-08-11 00:50:16 +0100

    changes

--- install/parse.go
+++ install/parse.go
@@ -1,11 +1,13 @@
 package install
 
-
-
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"path/filepath"
+	"PackageManager/config"
+	"io"
 )
 
 type Manifest struct {
@@ -41,7 +43,56 @@ func parseJson(filePath string){
 	// Example usage
 	fmt.Println("Version:", m.Version)
 	fmt.Println("Download URL:", m.Arch.X64.URL)
-	//fmt.Println(m.)
 
 
+	fileName := "btop4win-LHM-x64.zip"
+
+	targetPath := filepath.Join(config.APPS_DIR, fileName)
+
+
+	// Download
+	err = downloadFile(m.Arch.X64.URL, targetPath)
+
+	if err != nil {
+		fmt.Println("Download error:", err)
+		return
+	}
+
+	fmt.Println("File downloaded:", targetPath)
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+// downloadFile downloads from URL and saves to a file
+func downloadFile(url, filepath string) error {
+	// Send GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Copy data from response to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
